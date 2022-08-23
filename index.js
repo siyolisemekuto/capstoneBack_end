77c9e7ba045cec3ae0238f317601e75ac4422915
siyolisemekuto <siyozamekuto2.0@gmail.com>
2022-08-23 20:02:03 +0200

    added form

--- index.js
+++ index.js
@@ -20,6 +20,24 @@ app.get("/", (req,res) =>{
 app.get("/index", (req,res) =>{
  res.send(`
  <h1>Let's see if this works</h1>
+ <form>
+    <div class="row">
+        <div class="col-md-6 mt-3">
+            <input type="name" class="form-control" id="name" placeholder="Name" name="name"></div>
+        <div class="col-md-6 mt-3">
+            <input type="email" class="form-control" id="email" placeholder="Email" name="email"></div>
+        <div class="mt-3 ">
+            <input type="phone" class="form-control" id="phone" placeholder="Phone number" name="phone"></div>
+        </div>
+            <div class="row">
+                <div class=" mt-3 ">
+                    <textarea type="text" class="form-control" id="text" placeholder="Message" name="message"></textarea></div>
+                <div class="row display-flex justify-content-evenly">
+                    <div class="col-auto mt-3 mb-3">
+                        <button type="submit" class="btn" id="submit">Send Message</button>
+                      </div>
+                </div>
+</form>
  `)
 })
 
