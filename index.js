17619ad677ff473d3c27878136d8c39c77fe007a
siyolisemekuto <siyozamekuto2.0@gmail.com>
2022-08-23 10:41:44 +0200

    commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require ("express")
-cors = require ("cors")
+const express = require("express")
+cors = require("cors")
 const app = express()
 //app must get data from this port aka plug
 app.set("port", process.envPORT || 3000)
@@ -9,17 +9,17 @@ app.use(express.json());
 app.use(cors())
 
 //importing routes
-const usersRoutes = require ("./router/usersRoutes")
+const usersRoutes = require("./router/usersRoutes")
 
 //get to front-end
-app.get ("/", (req,res) =>{
+app.get("/", (req,res) =>{
     //msg will show database is connected
     res.json({msg:"We are here!!"})
 })
 
 //implementation of routes
 //route link (this file for this url)
-app.use ("/users", usersRoutes)
+app.use("/users", usersRoutes)
 
 app.listen(app.get("port"),() =>{
     console.log(`Listening for calls on port ${app.get("port")}`);
