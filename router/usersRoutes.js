b92a5ca6a7cc66f803b10befc98b96a7b8bbcd96
siyolisemekuto <siyozamekuto2.0@gmail.com>
2022-08-23 15:03:23 +0200

    nodemailer added

--- router/usersRoutes.js
+++ router/usersRoutes.js
@@ -2,6 +2,7 @@ const express = require ("express")
 const router = express.Router()
 const con = require ("../lib/db_connection")
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
 
 
 // Register Route
@@ -99,10 +100,9 @@ router.get("/", (req,res) =>{
 
 //delete user account
 router.delete('/:id', (req,res)=>{
-const {user_id}=req.body
         try{
             con.query(
-            `DELETE FROM users WHERE user_id="${user_id}"`, 
+            `DELETE FROM users WHERE user_id=${req.params.id}`, 
             (err, result) => {
                 if (err) throw err;
                 res.json(result);
@@ -115,10 +115,10 @@ const {user_id}=req.body
 
 //update user
 router.put('/:id', (req, res)=>{
-    const {user_id,name,email,password}= req.body
+    const {name,email,password}= req.body
         try{
             con.query(
-            `UPDATE users SET name="${name}",email="${email}",password="${password}" WHERE user_id=${user_id}`, 
+            `UPDATE users SET name="${name}",email="${email}",password="${password}" WHERE user_id=${req.params.id}`, 
             (err, result) => {
                 if (err) throw err;
                 res.json(result);
@@ -128,5 +128,121 @@ router.put('/:id', (req, res)=>{
             console.log(error);
         };
     })
+
+
+router.post('/forgot-psw', (req, res) => {
+    try {
+    let sql = `SELECT * FROM users WHERE ?`
+    let user = {
+      email: req.body.email,
+    };
+    con.query(sql, user, (err, result) => {
+      if (err) throw err;
+      if(result === 0) {
+        res.status(400), res.send("Email not found")
+      }
+      else {
+
+        // Allows me to connect to the given email account || Your Email
+        const transporter = nodemailer.createTransport({
+          host: 'smtp.ethereal.email',
+          port: 587,
+          auth: {
+              user: 'maia.sipes47@ethereal.email',
+              pass: 'nEmPHqWsCJHWW9N2Nu'
+          }
+      });
+
+        // How the email should be sent out
+      var mailData = {
+        from: process.env.MAILERUSER,
+        // Sending to the person who requested
+        to: result[0].email,
+
+        subject: 'Password Reset',
+        html:
+          `<div>
+            <h3>Hi ${result[0].name},</h3>
+            <br>
+            <h4>Click link below to reset your password</h4>
+
+            <a href="https://user-images.githubusercontent.com/4998145/52377595-605e4400-2a33-11e9-80f1-c9f61b163c6a.png">
+              Click Here to Reset Password
+              user_id = ${result[0].user_id}
+            </a>
+
+            <br>
+            <p>For any queries feel free to contact us...</p>
+            <div>
+              Email: ${process.env.MAILERUSER}
+            <div>
+          </div>`
+      };
+
+      // Check if email can be sent
+      // Check password and email given in .env file
+      transporter.verify((error, success) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email valid! ', success)
+        }
+      });
+
+      transporter.sendMail(mailData,  (error, info) => {
+        if (error) {
+          console.log(error);
+        } else {
+          res.send('Please Check your email', result[0].user_id)
+        }
+      });
+
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+
+// Reset Password Route
+router.put('reset-psw/:id', (req, res) => {
+  let sql = `SELECT * FROM users WHERE ?`;
+  let user = {
+    user_id: req.params.id,
+  };
+  con.query(sql, user, (err, result) => {
+    if (err) throw err;
+    if (result === 0) {
+      res.status(400), res.send("User not found");
+    } else {
+      let newPassword = `UPDATE users SET password=${password} WHERE user_id=${req.params.id}`;
+
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(req.body.password, salt);
+
+      const updatedPassword = {
+        full_name: result[0].full_name,
+        email: result[0].email,
+        user_type: result[0].user_type,
+        phone: result[0].phone,
+        country: result[0].country,
+        billing_address: result[0].billing_address,
+        default_shipping_address: result[0].default_shipping_address,
+
+        // Only thing im changing in table
+        password: hash,
+      };
+
+      con.query(newPassword, updatedPassword, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send("Password Updated please login");
+      });
+    }
+  });
+})
+
+
 
 module.exports = router
