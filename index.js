afb8673715887116018df3162e5f971aad082bd9
siyolisemekuto <siyozamekuto2.0@gmail.com>
2022-09-02 12:08:39 +0200

    removememnt

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json()); // Enable the server to handle JSON requests
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', '*');
   res.setHeader('Access-Control-Allow-Credentials', true);
   // handle OPTIONS method
   if ('OPTIONS' == req.method) {
