4d52dbfa352ce8e29bb2e18192d7dbb9d2263681
siyolisemekuto <siyozamekuto2.0@gmail.com>
2022-09-09 10:14:42 +0200

    replace put with patch

--- router/moodsRoutes.js
+++ router/moodsRoutes.js
@@ -31,7 +31,7 @@ router.post("/log-mood",middleware, (req, res) => {
   });
 
  //update moods
-router.put('/edit/:id',middleware, (req, res)=>{
+router.patch('/edit/:id', (req, res)=>{
   const {rating,notes}= req.body
       try{
           con.query(
